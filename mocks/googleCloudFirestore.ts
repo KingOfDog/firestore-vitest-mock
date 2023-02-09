575165e96073c93cdd98cce1a7ab22d594339bcb
KingOfDog <logins@kingofdog.de>
2023-02-09 14:19:36 +0100

    Remove remaining definition files

--- mocks/googleCloudFirestore.ts
+++ mocks/googleCloudFirestore.ts
@@ -1,13 +1,36 @@
 import { vi } from "vitest";
+import { FieldValue } from './fieldValue';
+import { StubOptions, StubOverrides } from './firebase.model';
+import { FakeFirestore } from './firestore';
+import { CollectionReference, DocumentReference } from './firestore.model';
 
 import defaultOptions from './helpers/defaultMockOptions';
+import { FieldPath } from './path';
+import { Query } from './query';
+import { Timestamp } from './timestamp';
+import { Transaction } from './transaction';
 
-export const firestoreStub = async (overrides, options = defaultOptions) => {
-  const { FakeFirestore, FakeAuth, Query, CollectionReference, DocumentReference, FieldValue, Timestamp, Transaction, FieldPath } = await import("firestore-vitest-mock");
+declare class Firestore extends FakeFirestore {
+  constructor();
+}
+
+interface GCloudFirestoreMock {
+  Firestore: Firestore;
+  Query: Query;
+  CollectionReference: CollectionReference;
+  DocumentReference: DocumentReference;
+  FieldValue: FieldValue;
+  FieldPath: FieldPath;
+  Timestamp: Timestamp;
+  Transaction: Transaction;
+}
+
+export const firestoreStub = async (overrides?: StubOverrides, options: StubOptions = defaultOptions): Promise<GCloudFirestoreMock> => {
+  const { FakeFirestore, Query, CollectionReference, DocumentReference, FieldValue, Timestamp, Transaction, FieldPath } = await import("firestore-vitest-mock");
 
   class Firestore extends FakeFirestore {
     constructor() {
-      super(overrides.database, options);
+      super(overrides?.database, options);
     }
   }
   return {
@@ -23,11 +46,11 @@ export const firestoreStub = async (overrides, options = defaultOptions) => {
   };
 };
 
-export const mockGoogleCloudFirestore = (overrides = {}, options = defaultOptions) => {
+export const mockGoogleCloudFirestore = (overrides?: StubOverrides, options: StubOptions = defaultOptions) => {
   mockModuleIfFound("@google-cloud/firestore", overrides, options);
 };
 
-function mockModuleIfFound(moduleName, overrides, options) {
+function mockModuleIfFound(moduleName: string, overrides?: StubOverrides, options?: StubOptions) {
   try {
     require.resolve(moduleName);
     vi.doMock(moduleName, () => firestoreStub(overrides, options));
--- mocks/reactNativeFirebaseFirestore.ts
+++ mocks/reactNativeFirebaseFirestore.ts
@@ -1,13 +1,30 @@
 import { vi } from "vitest";
+import { StubOptions, StubOverrides } from './firebase.model';
+import { CollectionReference, DocumentReference, FakeFirestore, FieldPath, FieldValue, Query, Timestamp, Transaction } from './firestore';
 
 import defaultOptions from './helpers/defaultMockOptions';
 
-export const firestoreStub = async (overrides, options = defaultOptions) => {
-  const { FakeFirestore, FakeAuth, Query, CollectionReference, DocumentReference, FieldValue, Timestamp, Transaction, FieldPath } = await import("firestore-vitest-mock");
+declare class Firestore extends FakeFirestore {
+  constructor();
+}
+
+interface GCloudFirestoreMock {
+  Firestore: Firestore;
+  Query: Query;
+  CollectionReference: CollectionReference;
+  DocumentReference: DocumentReference;
+  FieldValue: FieldValue;
+  FieldPath: FieldPath;
+  Timestamp: Timestamp;
+  Transaction: Transaction;
+}
+
+export const firestoreStub = async (overrides?: StubOverrides, options: StubOptions = defaultOptions): Promise<GCloudFirestoreMock> => {
+  const { FakeFirestore, Query, CollectionReference, DocumentReference, FieldValue, Timestamp, Transaction, FieldPath } = await import("firestore-vitest-mock");
 
   class Firestore extends FakeFirestore {
     constructor() {
-      super(overrides.database, options);
+      super(overrides?.database, options);
     }
   }
   return {
@@ -23,11 +40,11 @@ export const firestoreStub = async (overrides, options = defaultOptions) => {
   };
 };
 
-export const mockReactNativeFirestore = (overrides = {}, options = defaultOptions) => {
+export const mockReactNativeFirestore = (overrides?: StubOverrides, options: StubOptions = defaultOptions) => {
   mockModuleIfFound("@react-native-firebase/firestore", overrides, options);
 };
 
-function mockModuleIfFound(moduleName, overrides, options) {
+function mockModuleIfFound(moduleName: string, overrides?: StubOverrides, options?: StubOptions) {
   try {
     require.resolve(moduleName);
     vi.doMock(moduleName, () => firestoreStub(overrides, options));
